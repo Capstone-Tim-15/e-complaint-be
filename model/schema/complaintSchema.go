01d46d78a73b9e27211bd40919b422386845279e
Hafidz Naufal <91860668+hafidznaufl@users.noreply.github.com>
2023-12-01 20:09:51 +0700

    refa(complaint): modify response get method services

--- model/schema/complaintSchema.go
+++ model/schema/complaintSchema.go
@@ -12,6 +12,7 @@ type Complaint struct {
 	UpdatedAt   time.Time      `gorm:"autoUpdateTime:milli"`
 	DeletedAt   gorm.DeletedAt `gorm:"index"`
 	User_ID     string         `gorm:"column:user_id;size:191"`
+	User        User           `gorm:"foreignKey:User_ID;references:ID"`
 	Category_ID string         `gorm:"column:category_id;size:191"`
 	Category    Category       `gorm:"foreignKey:Category_ID;references:ID"`
 	Title       string
--- utils/response/complaintConvertResponse.go
+++ utils/response/complaintConvertResponse.go
@@ -32,13 +32,15 @@ func ComplaintDomaintoComplaintResponse(complaint *domain.Complaint) web.Complai
 
 func FindComplaintDomaintoComplaintResponse(complaint *domain.Complaint) web.ComplaintResponse {
 	complaintResponse := web.ComplaintResponse{
-		ID:       complaint.ID,
-		User_ID:  complaint.User_ID,
-		Category: complaint.Category.Name,
-		Title:    complaint.Title,
-		Content:  complaint.Content,
-		Status:   complaint.Status,
-		ImageUrl: complaint.ImageUrl,
+		ID:         complaint.ID,
+		User_ID:    complaint.User_ID,
+		Name:       complaint.User.Name,
+		PhotoImage: complaint.User.ImageUrl,
+		Category:   complaint.Category.Name,
+		Title:      complaint.Title,
+		Content:    complaint.Content,
+		Status:     complaint.Status,
+		ImageUrl:   complaint.ImageUrl,
 
 		Comment: make([]web.CommentResponse, len(complaint.Comment)),
 	}
@@ -61,13 +63,15 @@ func FindStatusComplaintDomaintoComplaintResponse(complaints []domain.Complaint)
 	var resultComplaintResponse []web.ComplaintResponse
 	for _, complaint := range complaints {
 		complaintResponse := web.ComplaintResponse{
-			ID:       complaint.ID,
-			User_ID:  complaint.User_ID,
-			Category: complaint.Category.Name,
-			Title:    complaint.Title,
-			Content:  complaint.Content,
-			Status:   complaint.Status,
-			ImageUrl: complaint.ImageUrl,
+			ID:         complaint.ID,
+			User_ID:    complaint.User_ID,
+			Name:       complaint.User.Name,
+			PhotoImage: complaint.User.ImageUrl,
+			Category:   complaint.Category.Name,
+			Title:      complaint.Title,
+			Content:    complaint.Content,
+			Status:     complaint.Status,
+			ImageUrl:   complaint.ImageUrl,
 
 			Comment: make([]web.CommentResponse, len(complaint.Comment)),
 		}
@@ -90,13 +94,15 @@ func ConvertComplaintResponse(complaints []domain.Complaint) []web.ComplaintResp
 	var results []web.ComplaintResponse
 	for _, complaint := range complaints {
 		complaintResponse := web.ComplaintResponse{
-			ID:       complaint.ID,
-			User_ID:  complaint.User_ID,
-			Category: complaint.Category.Name,
-			Title:    complaint.Title,
-			Content:  complaint.Content,
-			Status:   complaint.Status,
-			ImageUrl: complaint.ImageUrl,
+			ID:         complaint.ID,
+			User_ID:    complaint.User_ID,
+			Name:       complaint.User.Name,
+			PhotoImage: complaint.User.ImageUrl,
+			Category:   complaint.Category.Name,
+			Title:      complaint.Title,
+			Content:    complaint.Content,
+			Status:     complaint.Status,
+			ImageUrl:   complaint.ImageUrl,
 		}
 		results = append(results, complaintResponse)
 	}
