ec5e9e53e23dab69cbdcbd9101456ca6e0f04636
Syahdan Fauzi Utama <rootsyahdan111@gmail.com>
2023-12-15 17:54:09 +0700

    chore : modify route news,like,feedback from "users" to "user"

--- model/web/newsRequest.go
+++ model/web/newsRequest.go
@@ -6,7 +6,7 @@ import (
 
 type NewsCreateRequest struct {
 	Admin_ID    string
-	Category_ID string    `json:"categoryId" form:"categoryId" validate:"min=6,max=6"`
+	Category_ID string    `json:"categoryId" form:"categoryId" validate:"omitempty,min=6,max=6"`
 	Title       string    `json:"title" form:"title" validate:"required,min=1,max=255"`
 	Content     string    `json:"content" form:"content" validate:"required,min=1,max=255"`
 	Date        time.Time `json:"date" form:"date"`
